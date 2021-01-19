0f1dec642372edd85accde4bca3032104239df80
macterwilliam0504@gmail.com <williamlin@Williamde-MacBook-Pro-2.local>
2021-01-19 20:26:17 +0800

    OcfId v2

--- lib/openapi/models/model_guami.go
+++ lib/openapi/models/model_guami.go
@@ -12,4 +12,5 @@ package models
 type Guami struct {
 	PlmnId *PlmnId `json:"plmnId" yaml:"plmnId" bson:"plmnId" mapstructure:"PlmnId"`
 	AmfId  string  `json:"amfId" yaml:"amfId" bson:"amfId" mapstructure:"AmfId"`
+	OcfId  string  `json:"ocfId" yaml:"ocfId" bson:"ocfId" mapstructure:"OcfId"`
 }
