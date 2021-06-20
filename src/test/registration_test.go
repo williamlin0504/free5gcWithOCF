6a422365a07f43297503bf94948cfee7ca7e02ee
macterwilliam0504@gmail.com <williamlin@Williamde-MacBook-Pro-2.local>
2021-06-20 13:30:04 +0800

    Some error fixed

--- src/test/registration_test.go
+++ src/test/registration_test.go
@@ -332,6 +332,11 @@ func Nchf_ConvergedChargingFunction_create(ue_ID string){
     }
 	log.Println("GU Authorized.")
 
+	body, err := ioutil.ReadAll(resp.Body)
+   	if err != nil {
+    	  log.Fatalln(err)
+   	}
+
 	sb := string(body)
    	log.Printf(sb)
 
