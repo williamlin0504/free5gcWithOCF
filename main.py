c6274fee042090528150d41351433d9fbbcb4bbb
macterwilliam0504@gmail.com <williamlin@Williamde-MacBook-Pro-2.local>
2021-07-05 21:51:16 +0800

    Test Environment Variable

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask
+import os 
+
+
+app = Flask(__name__)
+@app.route('/<ueID>')
+
+def testSH(ue_ID):
+    os.system(' test.sh {} {}' .format('TestRegistration', ueID))   
+    return 'Test Started!'
+
+if __name__ == '__main__':
+  app.run(host="0.0.0.0",port=8080)
